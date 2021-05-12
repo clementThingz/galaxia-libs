ba7370d305b9ad1361d63f5efddcbe85b90bab7a
clement <clement@thingz.co>
2021-05-12 16:28:34 +0200

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Thingz'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme",'myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ["sphinx_rtd_theme",'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
